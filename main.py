f81408e4ebc24bea6470d148b6f7b237bcd568d6
“Surrin1999 <605308631@qq.com>
2021-12-01 22:41:43 +0800

    删除了检测模型时不必要的代码

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-import numpy
-import numpy as np
 from model.model_forecast import model
 from data.data_process import dataProcess
 import sys
@@ -14,9 +12,3 @@ model.save("my_model")
 saved_model = model.load_model("my_model")
 
 res_data = saved_model.test('/home/data/test_data.csv')
-
-
-def get_res(data):
-    res_data = saved_model.predict(data)
-
-    return str(np.round_(res_data)).replace("[", "").replace("]", "").replace(".", "")
